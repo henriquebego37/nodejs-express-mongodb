7efd0162277688747b64dd87c9282298a822f346
henriquebego37 <henriquedev37@gmail.com>
2025-02-04 20:43:14 -0300

    fix: initial structure

---
+++ server.js
@@ -0,0 +1,17 @@
+import http from "http";
+
+const PORT = 3000;
+
+const rotas = {
+      "/": "curso de node.js",
+      "/livros": "entrei na rota livros"
+};
+
+const server = http.createServer((req, res) => {
+      res.writeHead(200, { "Content-Type": "text/plain" }) // cabeçalho de uma requisição
+      res.end(rotas[req.url])
+});
+
+server.listen(PORT, () => { // evento que monitora o servidor. 
+      console.log(`server running in port => ${PORT}`)
+});
